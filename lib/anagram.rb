235976f408c49ae5858c7e81bb8bd07330103879
Mwenje <mwenje24@gmail.com>
2022-08-19 10:38:58 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :words
+    def initialize(words)
+        @words= words
+    end
+
+    def match(array)
+        array.filter do |item|
+            item.chars.sort == words.chars.sort
+        end
+    end
+end
